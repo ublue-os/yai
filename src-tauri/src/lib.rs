3c34e5ed8f84aa75444ecbddde85e4d02135205b
Zeglius <33781398+Zeglius@users.noreply.github.com>
2025-03-23 20:52:27 +0100

    chore: Replace dbus with lsblk for disk fetching (#2)
    * chore: fetch disks with tauri shell plugin and lsblk
    
    Signed-off-by: Zeglius <33781398+Zeglius@users.noreply.github.com>
    
    * chore: Use index for card id
    
    Signed-off-by: Zeglius <33781398+Zeglius@users.noreply.github.com>
    
    ---------
    
    Signed-off-by: Zeglius <33781398+Zeglius@users.noreply.github.com>

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,27 +1,40 @@
-use zbus::zvariant;
-
+use std::process::Command;
 
 // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
 
+/// Outputs a JSON with this schema:
+/// ```json
+/// {
+///     "blockdevices": [
+///        {
+///           "name": "/dev/sda",
+///           "size": "894,3G"
+///        }
+///     ]
+///  }
+/// ```
 #[tauri::command]
-async fn get_blockdevices() -> Vec<String> {
-    let connection =  zbus::Connection::system().await.expect("Failure connecting to system dbus");
-    let udisks = zbus::Proxy::new(
-        &connection, "org.freedesktop.UDisks2", "/org/freedesktop/UDisks2/Manager", "org.freedesktop.UDisks2.Manager").await.expect("Failed getting UDisks DBus interface");
-    let block_devices = udisks.call_method("GetBlockDevices", &std::collections::HashMap::<String, zvariant::Value>::new()).await.expect("Failed getting block devices");
-    let body = block_devices.body();
-    let resp: Vec<zvariant::ObjectPath> = body.deserialize().expect("Failed getting devices from dbus response");
-    resp.iter().map(|e| e.to_string()).collect()
+async fn get_blockdevices() -> String {
+    let output = Command::new("lsblk")
+        .arg("-p")
+        .arg("-n")
+        .arg("--json")
+        .arg("--filter='TYPE == \"disk\"'")
+        .arg("-o")
+        .arg("NAME,SIZE")
+        .output()
+        .expect("failed to fetch disks")
+        .stdout;
+    let output_str = String::from_utf8(output).expect("failed to parse output as UTF-8");
+    return dbg!(output_str);
 }
-
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
+        .plugin(tauri_plugin_shell::init())
         .plugin(tauri_plugin_opener::init())
-        .invoke_handler(tauri::generate_handler![
-            get_blockdevices
-        ])
+        .invoke_handler(tauri::generate_handler![get_blockdevices])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
