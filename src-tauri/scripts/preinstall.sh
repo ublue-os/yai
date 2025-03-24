63ccc15691953efa75b1b98b86be7a691605cdfc
Niklas Haiden <niklas.haiden@gmail.com>
2025-03-25 00:25:41 +0100

    feat: get installer somewhat working

---
+++ src-tauri/scripts/preinstall.sh
@@ -0,0 +1,6 @@
+echo "-------------"
+echo "This is pre"
+echo "Install Value: $1"
+echo "Upgrade Value: $1"
+echo "Uninstall Value: $1"
+echo "-------------"
---
+++ src/lib/useDiskStore.ts
@@ -0,0 +1,6 @@
+import { create } from 'zustand'
+
+export const useDiskStore = create((set) => ({
+    disk: "",
+    setDisk: (disk: string) => set({ disk }),
+}))
